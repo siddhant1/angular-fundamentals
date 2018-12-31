33b162f07d640b948320ef7452b0bb2e5a396b48
siddhant1 <sidhant.manchanda@gmail.com>
2018-12-31 19:05:23 +0530

    ngFor and ngIf added with ng-template

--- angular-fundamentals/src/app/app.component.ts
+++ angular-fundamentals/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+interface Passenger {
+  id: number;
+  fullname: string;
+  checkedIn: boolean;
+}
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-fundamentals';
+  name: String = "";
+  passengers: Passenger[] = [
+    {
+      id: 1,
+      fullname: "Stephen",
+      checkedIn: true
+    },
+    {
+      id: 2,
+      fullname: "Rose",
+      checkedIn: false
+    },
+    {
+      id: 3,
+      fullname: "James",
+      checkedIn: true
+    },
+    {
+      id: 4,
+      fullname: "Louise",
+      checkedIn: true
+    },
+    {
+      id: 5,
+      fullname: "Tina",
+      checkedIn: false
+    }
+  ];
+  handleChange(value: String) {
+    this.name = value;
+  }
 }
