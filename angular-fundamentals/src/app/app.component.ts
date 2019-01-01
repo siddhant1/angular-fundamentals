500a300f356b495180426dbc2a2f11c2bddea375
siddhant1 <sidhant.manchanda@gmail.com>
2019-01-01 15:56:51 +0530

    pipe and pipe chaining

--- angular-fundamentals/src/app/app.component.ts
+++ angular-fundamentals/src/app/app.component.ts
@@ -4,8 +4,9 @@ interface Passenger {
   id: number;
   fullname: string;
   checkedIn: boolean;
+  checkInDate?: number;
 }
-@Component({ 
+@Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
@@ -16,27 +17,32 @@ export class AppComponent {
     {
       id: 1,
       fullname: "Stephen",
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1490742000000
     },
     {
       id: 2,
       fullname: "Rose",
-      checkedIn: false
+      checkedIn: false,
+      checkInDate: null
     },
     {
       id: 3,
       fullname: "James",
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1491606000000
     },
     {
       id: 4,
       fullname: "Louise",
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1488412800000
     },
     {
       id: 5,
       fullname: "Tina",
-      checkedIn: false
+      checkedIn: false,
+      checkInDate: null
     }
   ];
   handleChange(value: String) {
