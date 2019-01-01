b84b37a4533ad9d980f80c537c365a8327eda87e
siddhant1 <sidhant.manchanda@gmail.com>
2019-01-01 18:41:59 +0530

    Safe navigation opertor

--- angular-fundamentals/src/app/app.component.ts
+++ angular-fundamentals/src/app/app.component.ts
@@ -5,7 +5,14 @@ interface Passenger {
   fullname: string;
   checkedIn: boolean;
   checkInDate?: number;
+  children?: Child[];
 }
+
+interface Child {
+  name: string;
+  age: number;
+}
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -18,31 +25,36 @@ export class AppComponent {
       id: 1,
       fullname: "Stephen",
       checkedIn: true,
-      checkInDate: 1490742000000
+      checkInDate: 1490742000000,
+      children: null
     },
     {
       id: 2,
       fullname: "Rose",
       checkedIn: false,
-      checkInDate: null
+      checkInDate: null,
+      children: [{ name: "Ted", age: 12 }, { name: "Chloe", age: 7 }]
     },
     {
       id: 3,
       fullname: "James",
       checkedIn: true,
-      checkInDate: 1491606000000
+      checkInDate: 1491606000000,
+      children: null
     },
     {
       id: 4,
       fullname: "Louise",
       checkedIn: true,
-      checkInDate: 1488412800000
+      checkInDate: 1488412800000,
+      children: [{ name: "Jessica", age: 1 }]
     },
     {
       id: 5,
       fullname: "Tina",
       checkedIn: false,
-      checkInDate: null
+      checkInDate: null,
+      children: null
     }
   ];
   handleChange(value: String) {
